186c69f70ea4868406c5f215b198e4fe76e8c5ea
Shchuka-Vladimir <vovtut.by@gmail.com>
2023-02-27 22:43:32 +0300

    v.shchuka/homework_1 short js code

--- src/v.shchuka/homework/homework_1/js/script.js
+++ src/v.shchuka/homework/homework_1/js/script.js
@@ -1,9 +1,6 @@
 const elementBreakfast = document.querySelector(".breakfast");
 const elementUpArrow = document.querySelector(".arrow-up");
-const elementBreakfastList = document.querySelector(".breakfast-list");
 elementUpArrow.addEventListener("click", hideList);
 function hideList() {
-  elementBreakfast.classList.toggle("show-radius");
-  elementUpArrow.classList.toggle("arrow-up--rotate");
-  elementBreakfastList.classList.toggle("hide-list");
+  elementBreakfast.classList.toggle("breakfast--radius");
 }
