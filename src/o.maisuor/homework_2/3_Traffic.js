e6cfa949ab1566cfe3943a021a4a5ec68f6755af
omar <1986maisuromar>
2023-03-03 16:16:29 +0300

    3 task solved

---
+++ src/o.maisuor/homework_2/3_Traffic.js
@@ -0,0 +1,13 @@
+function updateLight(current) {
+  
+if (current === 'green'){
+    return('yellow');
+    } else if (current === 'yellow'){
+    return('red');
+    } else{
+    return('green');
+    }
+      
+    
+
+}
