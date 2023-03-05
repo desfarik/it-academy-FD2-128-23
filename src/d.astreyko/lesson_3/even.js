25d3b2bafbf44c1271d12c335bb5093de4e108fc
VolhaTsv <volhatsv@gmail.com>
2023-03-05 17:05:12 +0300

    Added files Added 15 files

--- src/d.astreyko/lesson_3/even.js
+++
@@ -1,7 +0,0 @@
-const num = +prompt('Введи число');
-
-for (let i = 0; i<=num; i++) {
-  if(i % 2 === 0) {
-    console.log(i);
-  }
-}
--- src/i.dmitrakova/lesson2/num.js
+++
@@ -1,7 +0,0 @@
-const str = '40.9666666666px';
-
-console.log(str);
-console.log(Number(str));
-console.log(+str);
-console.log(parseInt(str, 10));
-console.log(parseFloat(str));
