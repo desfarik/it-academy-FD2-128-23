e23d732a942f4b69feebb55cdc07e816eb241cc6
omar <1986maisuromar>
2023-03-05 19:47:52 +0300

    from task 8 to task 16

---
+++ src/o.maisuor/homeworkk_2/13_Count chars.js
@@ -0,0 +1,18 @@
+function consonantCount(str) {
+    const counter = 0
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    if (str.includes(vowels)){
+      counter++
+    }
+  return counter;
+      let word = str.toLowerCase()
+  //     if(/[aeiouy]/.test(word)){
+  //    return counter++
+  // } 
+  // else {
+  //    return 0
+  // }
+ 
+}
+
+console.log(consonantCount(abcdefghijkopru))
