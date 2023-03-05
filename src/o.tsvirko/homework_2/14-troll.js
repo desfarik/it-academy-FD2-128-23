fa34d4c196e8f273b5af52816ee5b2f631838f7d
VolhaTsv <volhatsv@gmail.com>
2023-03-05 16:44:51 +0300

    Added homework
    15 files

--- src/o.tsvirko/homework_2/14-troll.js
+++
@@ -1,4 +0,0 @@
-function disemvowel(str) {
-    return str.replace(/[aeiou]/gi, '');
-}
-console.log(disemvowel('Aaeksdnf sdfn dsfsaeee?'));
--- src/o.tsvirko/homework_2/5-triangle-angle.js
+++
@@ -1,5 +0,0 @@
-function otherAngle(a, b) {
-    let c = 180 - (a + b);
-    return c;
-}
-console.log(otherAngle(30, 45));
