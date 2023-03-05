c9bde8d79c9640a1b072e4bfbb8446570a8886e7
Shchuka-Vladimir <vovtut.by@gmail.com>
2023-03-05 23:51:43 +0300

    v.shchuka/homework_2 simplified task 17

--- src/v.shchuka/homework/homework_2/17-sort-by-last-char.js
+++ src/v.shchuka/homework/homework_2/17-sort-by-last-char.js
@@ -1,13 +1,10 @@
 function last(x) {
   let arrStr = x.split(" ");
-  let lastSymb = [];
   let lengthArr = arrStr.length;
   while (lengthArr) {
     for (let i = 0; i < lengthArr - 1; i++) {
       const str = arrStr[i];
-      lastSymb[i] = str.at(-1);
-      lastSymb[i + 1] = arrStr[i + 1].at(-1);
-      if (lastSymb[i + 1] < lastSymb[i]) {
+      if (arrStr[i + 1].at(-1) < str.at(-1)) {
         arrStr[i] = arrStr[i + 1];
         arrStr[i + 1] = str;
       }
