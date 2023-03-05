f57dc6728005ad7a7af9e8c6d2a388340ce4337c
Aleksey-Samoylo <SamoyloAleksey@outlook.com>
2023-03-05 23:15:50 +0300

    last version add some tests

--- src/a.samoylo/homework_2/15-parseFlout.js
+++ src/a.samoylo/homework_2/15-parseFlout.js
@@ -2,9 +2,9 @@ function newParseFloat(str) {
     return isNaN(str[0]) ? NaN : +str.match(/(\d+\.\d+)/g) === 0 ? str.match(/\d+/) * 1 : str.match(/(\d+\.\d+)/g) * 1;
 }
 
-console.log(newParseFloat('563478.34534sdfsdf'));
-console.log(newParseFloat('2342'));
-console.log(newParseFloat('23423.4324 sdfsdf'));
-console.log(newParseFloat('gfd234234'));
-console.log(newParseFloat('sdfsdf25.523'));
-console.log(newParseFloat('4324.234.234fdss'));
+console.log(newParseFloat('563478.34534sdfsdf') === 563478.34534);
+console.log(newParseFloat('2342') === 2342);
+console.log(newParseFloat('23423.4324 sdfsdf') === 23423.4324);
+console.log(isNaN(newParseFloat('gfd234234'))); // В parseFloat если str начинается с бука, отдаёт NaN
+console.log(isNaN(newParseFloat('sdfsdf25.523'))); // В parseFloat если str начинается с бука, отдаёт NaN
+console.log(newParseFloat('4324.234.234fdss') === 4324.234);
--- src/a.samoylo/homework_2/18-Round.js
+++ src/a.samoylo/homework_2/18-Round.js
@@ -4,9 +4,9 @@ function round(num) {
     `${num}`.split('.')[1] < 5 || `${num}`.split('.')[0] == 0 ? `${num}`.split('.')[0] : +`${num}`.split('.')[0] - 1;
 };
 
-console.log(round(12434.56666));
-console.log(round(2.9));
-console.log(round(0));
-console.log(round(-2.9));
-console.log(round(2.5)); // если .5 то округляю в большую сторону
-console.log(round(0.1));
+console.log(round(12434.56666) === 12435);
+console.log(round(2.9) === 3);
+console.log(round(0) == 0);
+console.log(round(-2.9) === -3);
+console.log(round(2.5) === 3); // если .5 то округляю в большую сторону
+console.log(round(0.1) == 0);
