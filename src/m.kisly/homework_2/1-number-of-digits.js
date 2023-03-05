fe1c3e7108bad0e09e335e1526f6d3d7edde4d28
Max <Mkisly@bk.ru>
2023-03-05 21:47:11 +0300

    m.kisly/homework_2

---
+++ src/m.kisly/homework_2/1-number-of-digits.js
@@ -0,0 +1,6 @@
+function digits(n) {
+  return String(n).length
+}
+console.log(digits(5));           //1
+console.log(digits(12345));       //5
+console.log(digits(9876543210));  //10
---
+++ src/m.kisly/homework_2/6-car-renting.js
@@ -0,0 +1,8 @@
+function rentalCarCost(d) {
+  if (d > 6) return (d * 40) - 50;
+  if (d > 2) return (d * 40) - 20;
+  else
+    return d * 40;
+}
+console.log(rentalCarCost(1));  //40
+console.log(rentalCarCost(2));  //80
