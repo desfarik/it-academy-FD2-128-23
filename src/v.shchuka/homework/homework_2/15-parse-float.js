4400144856b43625e4ccb4159b53a2a6de4ebe04
Shchuka-Vladimir <vovtut.by@gmail.com>
2023-03-06 07:57:27 +0300

    v.shchuka/homework_2 simplified task 15

--- src/v.shchuka/homework/homework_2/15-parse-float.js
+++ src/v.shchuka/homework/homework_2/15-parse-float.js
@@ -1,19 +1,10 @@
 function parseFloat(str) {
   const digits = "0123456789";
   const dot = ".";
-  let leftSubstr;
-  let indexSymb;
-  let rightSubstr;
   let result = "";
-  for (let i = 0; i < str.length; i++) {
-    const symb = str[i];
-    if (symb === dot) {
-      indexSymb = str.indexOf(symb);
-      leftSubstr = str.slice(0, indexSymb);
-      rightSubstr = str.slice(indexSymb + 1);
-      break;
-    }
-  }
+  const indexSymb = str.indexOf(dot);
+  const leftSubstr = str.slice(0, indexSymb);
+  const rightSubstr = str.slice(indexSymb + 1);
   for (const symb of leftSubstr) {
     if (digits.includes(symb)) {
       result += symb;
