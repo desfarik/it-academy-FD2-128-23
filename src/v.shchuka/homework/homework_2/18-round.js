cd12782437f1ad9e8296ae53d56d804b69c9d781
Shchuka-Vladimir <vovtut.by@gmail.com>
2023-03-06 22:22:42 +0300

    homework_2

--- src/v.shchuka/homework/homework_2/18-round.js
+++ src/v.shchuka/homework/homework_2/18-round.js
@@ -1,13 +1,14 @@
 function round(num) {
   const str = `${num}`;
+  if (isNaN(num)) {
+    return NaN;
+  }
   return str.includes(".") ? roundNum(str, num) : +num;
   function roundNum(str, num) {
     const indexDot = str.indexOf(".");
     const floatPart = str.slice(indexDot + 1);
     const integerPart = +str.slice(0, indexDot);
-    if (floatPart != +floatPart) {
-      return NaN;
-    } else if (floatPart[0] < 5) {
+    if (floatPart[0] < 5 || floatPart === "") {
       return integerPart;
     } else {
       return num < 0 ? integerPart - 1 : integerPart + 1;
