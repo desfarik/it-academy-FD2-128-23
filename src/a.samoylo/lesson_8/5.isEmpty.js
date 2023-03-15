e65221c60003f6744f1578dbba7dd09b45542025
Aleksey-SAmoylo <SamoyloAleksey@outlook.com>
2023-03-15 21:54:13 +0300

    lesson 8

---
+++ src/a.samoylo/lesson_8/5.isEmpty.js
@@ -0,0 +1,9 @@
+function isEmpty(object) {
+    return Object.keys(object).length===0
+}
+console.log(isEmpty({'name': 'hello'}))
+const obj = {
+    'name': 'hello'
+}
+console.log(obj.name)
+console.log(obj.hi)
