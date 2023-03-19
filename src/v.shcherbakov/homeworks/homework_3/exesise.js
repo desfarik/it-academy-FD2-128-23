fb2491d5613d8d06c89aeea88bbc709e35d722ab
Vitaliy24011980 <6663213@gmail.com>
2023-03-19 15:12:22 +0300

    v.shcherbakov/lesson_9

---
+++ src/v.shcherbakov/homeworks/homework_3/exesise.js
@@ -0,0 +1,33 @@
+function filterEven(array) {
+    array.forEach(function (item, index, arr){
+        console.log(index,item, arr);
+    })
+    array.forEach((item, index, arr) => {
+        console.log(index,item, arr);
+    })
+//const arrayX2 = array.map(item => item * 2)
+    const arrayX2 = array.map(function (item, index, arr){
+        return item * 2
+    })
+    console.log(arrayX2);
+// array.flatMap()
+    const evens = array.filter(function (item, index, arr){
+        return item % 2 === 0
+    })
+    console.log(evens);
+    const allEvery = evens.every(function (item, index, arr){
+        return item % 2 === 0
+    })
+    console.log(allEvery);
+    // array.some()
+    /*   array.reduce()*/
+    const result = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            result.push(array[i]);
+        }
+    }
+    return result;
+}
+
+console.log(filterEven([1,4,8,9]));
