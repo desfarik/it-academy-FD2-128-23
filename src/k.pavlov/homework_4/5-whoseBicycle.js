e4051dfa9712ac8b402cf6c5a84fa48d4fd2eeb9
Klim <Klimpavlov2002@gmail.com>
2023-03-20 13:27:57 +0300

    lesson 8

--- src/k.pavlov/homework_4/5-whoseBicycle.js
+++ src/k.pavlov/homework_4/5-whoseBicycle.js
@@ -1,12 +1,17 @@
 function whoseBicycle(diary1, diary2, diary3) {
+    let newDiary1 = Object.values(diary1).reduce((acc, number) => acc + number, 0);
+    let newDiary2 = Object.values(diary2).reduce((acc, number) => acc + number, 0);
+    let newDiary3 = Object.values(diary3).reduce((acc, number) => acc + number, 0);
 
-    let sum = 0
-    for (const key in diary1) {
-        sum += diary1[key]
+    if (newDiary1 > newDiary2 && newDiary1 > newDiary3) {
+        return 'I need to buy a bicycle for my first son.';
+    } else if (newDiary2 >= newDiary1 && newDiary2 > newDiary3) {
+        return 'I need to buy a bicycle for my second son.';
+    } else if (newDiary3 >= newDiary1 && newDiary3 >= newDiary2) {
+        return 'I need to buy a bicycle for my third son.';
     }
-    return sum
-
 }
+
 console.log(whoseBicycle({
         'algebra': 6,
         'history': 7,
--- src/k.pavlov/homework_4/6-sentence.js
+++ src/k.pavlov/homework_4/6-sentence.js
@@ -1,7 +1,10 @@
 function sentence(List) {
-        let keys = List.reduce((container, obj) => [...container, ...Object.keys(obj)], []);
-        return keys.sort((a, b) => a - b);
+    //return List.reduce((container, obj) => [...container, ...Object.keys(obj)], []).sort((a, b) => a - b).map((values) => Object.values(values)).join(' ');
+
+    let sorted = List.sort((a, b) => Object.keys(a) - Object.keys(b));
+    return sorted.map((values) => Object.values(values)).join(' ');
 }
+
 console.log(sentence([
     {'4': 'dog' }, {'2': 'took'}, {'3': 'his'},
     {'-2': 'Vatsan'}, {'5': 'for'}, {'6': 'a'}, {'12': 'spin'}
