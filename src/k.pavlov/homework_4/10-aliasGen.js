0b2c8302ce076617ff5889d5cf771967db46c878
Klim <Klimpavlov2002@gmail.com>
2023-03-20 13:27:57 +0300

    lesson 8

--- src/k.pavlov/homework_4/10-aliasGen.js
+++ src/k.pavlov/homework_4/10-aliasGen.js
@@ -1,7 +1,57 @@
-function aliasGen() {
-    let firstName = {A: 'Alpha', B: 'Beta', C: 'Cache'}
-    let surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst'}
-    return Object.values(firstName)
-}
 
-console.log(aliasGen())
+
+
+
+console.log(aliasGen({ A: 'Alpha',
+    B: 'Beta',
+    C: 'Cache',
+    D: 'Data',
+    E: 'Energy',
+    F: 'Function',
+    G: 'Glitch',
+    H: 'Half-life',
+    I: 'Ice',
+    J: 'Java',
+    K: 'Keystroke',
+    L: 'Logic',
+    M: 'Malware',
+    N: 'Nagware',
+    O: 'OS',
+    P: 'Phishing',
+    Q: 'Quantum',
+    R: 'RAD',
+    S: 'Strike',
+    T: 'Trojan',
+    U: 'Ultraviolet',
+    V: 'Vanilla',
+    W: 'WiFi',
+    X: 'Xerox',
+    Y: 'Y',
+    Z: 'Zero' },
+
+    { A: 'Analogue',
+    B: 'Bomb',
+    C: 'Catalyst',
+    D: 'Discharge',
+    E: 'Electron',
+    F: 'Faraday',
+    G: 'Gig',
+    H: 'Hacker',
+    I: 'IP',
+    J: 'Jabber',
+    K: 'Killer',
+    L: 'Lazer',
+    M: 'Mike',
+    N: 'n00b',
+    O: 'Overclock',
+    P: 'Payload',
+    Q: 'Quark',
+    R: 'Roy',
+    S: 'Spy',
+    T: 'T-Rex',
+    U: 'Unit',
+    V: 'Virus',
+    W: 'Worm',
+    X: 'X',
+    Y: 'Yob',
+    Z: 'Zombie' }));
--- src/k.pavlov/homework_4/7-killer.js
+++ src/k.pavlov/homework_4/7-killer.js
@@ -1,16 +1,14 @@
 function killer(suspectInfo, dead) {
-    //return Object.values(dead)
-    // for (const key in suspectInfo) {
-    //     if (Object.values(dead) in Object.values(suspectInfo)) {
-    //        return  suspectInfo[key]
-    //     }
-    // }
-
-    if (Object.values(dead) in Object.values(suspectInfo)) {
-        return Object.keys(suspectInfo)
+    for (const key in suspectInfo) {
+        if (suspectInfo[key].includes(...dead)) {
+            return key
+        }
     }
 }
 
-console.log(killer({'James': ['Jacob', 'Bill', 'Lucas'],
-    'Johnny': ['David', 'Kyle', 'Lucas'],
-    'Peter': ['Lucy', 'Kyle']}, ['Lucas', 'Bill']))
+
+console.log(killer({
+    'James': ['Jacob'],
+    'Johnny': ['David', 'Kyle', 'Lucas', 'Bill'],
+    'Peter': ['Lucy', 'Kyle']
+}, ['Lucas', 'Bill']))
