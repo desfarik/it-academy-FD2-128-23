bf51f70e1f9f2d531abcb492186c90c54dace674
Dima Astreyko <desfarik@gmail.com>
2023-03-24 21:19:56 +0300

    lesson 11

--- src/d.astreyko/lesson_10/home.js
+++ src/d.astreyko/lesson_10/home.js
@@ -1,12 +1,27 @@
-const game= new Game();
+const game = new Game();
 
 while (!game.isEnd()) {
   game.askPlayers();
 }
 
-game.showWinners()
+game.showWinners();
 
-
-askForCard(){
-  promt(`Игрок ${this.name}. карты: [ 5♥, д♦] . Очков: 8. Хотите вытянуть еще одну карту?`)
+askForCard();
+{
+  promt(`Игрок ${this.name}. карты: [ 5♥, д♦] . Очков: 8. Хотите вытянуть еще одну карту?`);
 }
+
+for (let suit of suits) {
+  for (let card of cards) {
+    new Card(suit, card);
+  }
+}
+
+const CARD_SCORES = {
+  1: 1,
+  2: 2,
+  3: 3,
+  4: 4,
+  valet: 2,
+  dama: 3,
+};
