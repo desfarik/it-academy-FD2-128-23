15ead1b9f1f25d5b79e457e9cbd1c4b44f0ede3e
ViktorKameisha <viktor.kameisha.96@gmail.com>
2023-04-03 00:52:58 +0300

    v.kameisha/homeWork6

---
+++ src/v.kameisha/lesson13/lesson13.js
@@ -0,0 +1,18 @@
+// function generateTable(n){
+//     let table = document.querySelector('#table');
+//     for (let i = 0; i < n; i++) {
+//         let tr = document.createElement('tr');
+//         for (let i = 0; i < n; i++) {
+//             let td = document.createElement('td');
+//             tr.appendChild(td);
+//         }
+//         table.appendChild(tr);
+//     }
+//     let allTr = document.querySelectorAll('tr');
+//     for(let i = 0; i < allTr.length; i++){
+//         allTr[allTr.length - i - 1].children[i].style.background = 'blue';
+//     }
+
+    
+// }
+// console.log(generateTable(5));
