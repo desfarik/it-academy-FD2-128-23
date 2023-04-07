f33812d320c54c3d7e54ebd93a598505f25f2dd4
VolhaTsv <volhatsv@gmail.com>
2023-04-07 10:31:55 +0300

    added homework_6

---
+++ src/o.tsvirko/homework_6/js/data.js
@@ -0,0 +1,213 @@
+const SONG_ARRAY = [
+    {
+        id : 1,
+        img : "images/depeche-mode.jpg",
+        albumName : "Memento Mori",
+        author : "Depeche Mode",
+        songs : [
+            {
+                id : 11,
+                img : "images/depeche-mode.jpg",
+                songName : "My Cosmos Is Mine",
+                author : "Depeche Mode",
+                duration : "05:11"
+            },
+            {
+                id : 12,
+                img : "images/depeche-mode.jpg",
+                songName : "Wagging Tongue",
+                author : "Depeche Mode",
+                duration : "03:20"
+            },
+            {
+                id : 13,
+                img : "images/depeche-mode.jpg",
+                songName : "Ghosts Again",
+                author : "Depeche Mode",
+                duration : "04:00"
+            },
+            {
+                id : 14,
+                img : "images/depeche-mode.jpg",
+                songName : "Don&apos;t Say You Love Me",
+                author : "Depeche Mode",
+                duration : "03:56"
+            },
+            {
+                id : 15,
+                img : "images/depeche-mode.jpg",
+                songName : "My Favourite Stranger",
+                author : "Depeche Mode",
+                duration : "03:47"
+            }
+        ]
+    }, {
+        id : 2,
+        img : "images/self-deception.jpg",
+        albumName : "You Are Only As Sick As Your Secrets",
+        author : "Self Deception",
+        songs : [
+            {
+                id : 21,
+                img : "images/self-deception.jpg",
+                songName : "Holy Water",
+                author : "Self Deception",
+                duration : "03:37"
+            },
+            {
+                id : 22,
+                img : "images/self-deception.jpg",
+                songName : "Fight Fire With Gasoline",
+                author : "Self Deception",
+                duration : "03:48"
+            },
+            {
+                id : 23,
+                img : "images/self-deception.jpg",
+                songName : "State Of Elysium",
+                author : "Self Deception",
+                duration : "03:10"
+            },
+            {
+                id : 24,
+                img : "images/self-deception.jpg",
+                songName : "Eleven",
+                author : "Self Deception",
+                duration : "01:51"
+            },
+            {
+                id: 25,
+                img : "images/self-deception.jpg",
+                songName : "Stockholm Hearts",
+                author : "Self Deception",
+                duration : "03:17"
+            }
+        ]
+    }, {
+        id : 3,
+        img : "images/skillet.jpg",
+        albumName : "Dominion: Day of Destiny",
+        author : "Skillet",
+        songs : [
+            {
+                id : 31,
+                img : "images/skillet.jpg",
+                songName : "Surviving the Game",
+                author : "Skillet",
+                duration : "03:58"
+            },
+            {
+                id : 32,
+                img : "images/skillet.jpg",
+                songName : "Standing in the Storm",
+                author : "Skillet",
+                duration : "04:17"
+            },
+            {
+                id : 33,
+                img : "images/skillet.jpg",
+                songName : "Dominion",
+                author : "Skillet",
+                duration : "03:51"
+            },
+            {
+                id : 34,
+                img : "images/skillet.jpg",
+                songName : "Valley of Death",
+                author : "Skillet",
+                duration : "04:27"
+            },
+            {
+                id: 35,
+                img : "images/skillet.jpg",
+                songName : "Beyond Incredible",
+                author : "Skillet",
+                duration : "03:31"
+            }
+        ]
+    }, {
+        id : 4,
+        img : "images/theory-of-a-deadman.jpg",
+        albumName : "Dinosaur",
+        author : "Theory of a Dead Man",
+        songs : [
+            {
+                id : 41,
+                img : "images/theory-of-a-deadman.jpg",
+                songName : "Dinosaur",
+                author : "Theory of a Dead Man",
+                duration : "03:48"
+            },
+            {
+                id : 42,
+                img : "images/theory-of-a-deadman.jpg",
+                songName : "Medusa",
+                author : "Theory of a Dead Man",
+                duration : "03:39"
+            },
+            {
+                id : 43,
+                img : "images/theory-of-a-deadman.jpg",
+                songName : "Sick",
+                author : "Theory of a Dead Man",
+                duration : "03:22"
+            },
+            {
+                id : 44,
+                img : "images/theory-of-a-deadman.jpg",
+                songName : "Two Of Us",
+                author : "Theory of a Dead Man",
+                duration : "03:31"
+            },
+            {
+                id: 45,
+                img : "images/theory-of-a-deadman.jpg",
+                songName : "Ambulance",
+                author : "Theory of a Dead Man",
+                duration : "03:27"
+            }
+        ]
+    }, {
+        id : 5,
+        img : "images/godsmack.jpg",
+        albumName : "Lighting Up The Sky",
+        author : "Godsmack",
+        songs : [
+            {
+                id : 51,
+                img : "images/godsmack.jpg",
+                songName : "You and I",
+                author : "Godsmack",
+                duration : "05:17"
+            },
+            {
+                id : 52,
+                img : "images/godsmack.jpg",
+                songName : "Red White and Blue",
+                author : "Godsmack",
+                duration : "04:04"
+            },
+            {
+                id : 53,
+                img : "images/godsmack.jpg",
+                songName : "Surrender",
+                author : "Godsmack",
+                duration : "03:40"
+            },
+            {
+                id : 54,
+                img : "images/godsmack.jpg",
+                songName : "What About Me",
+                author : "Godsmack",
+                duration : "03:55"
+            },
+            {
+                id: 55,
+                img : "images/godsmack.jpg",
+                songName : "Truth",
+                author : "Godsmack",
+                duration : "04:33"
+            }
+        ]
+    },
+]
