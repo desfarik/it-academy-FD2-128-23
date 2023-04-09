ec9c8b30468da5f18f2e28840995da96e2dbee7c
yuri_sn <yuri.snitko@gmail.com>
2023-04-09 22:43:58 +0300

    memory game

---
+++ src/y.snitko/homework_7/script/data.js
@@ -0,0 +1,50 @@
+const cards = [
+  {
+    id: 1,
+    src: './img/basketball.svg',
+  },
+  {
+    id: 2,
+    src: './img/beer.svg',
+  },
+  {
+    id: 3,
+    src: './img/bus.svg',
+  },
+  {
+    id: 4,
+    src: './img/compass.svg',
+  },
+  {
+    id: 5,
+    src: './img/house.svg',
+  },
+  {
+    id: 6,
+    src: './img/macbook.svg',
+  },
+  {
+    id: 7,
+    src: './img/rocket.svg',
+  },
+  {
+    id: 8,
+    src: './img/star.svg',
+  },
+  {
+    id: 9,
+    src: './img/thumb.svg',
+  },
+  {
+    id: 10,
+    src: './img/tree.svg',
+  },
+  {
+    id: 11,
+    src: './img/plane.svg',
+  },
+  {
+    id: 12,
+    src: './img/target.svg',
+  },
+];
