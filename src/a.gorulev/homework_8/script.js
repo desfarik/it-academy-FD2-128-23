01cfb113841e24db2893cecb806080262e4fa270
Alex-from-the-mountains <alexgoruliov@gmail.com>
2023-04-15 18:52:48 +0300

    a.gorulev/homework_8

--- src/a.gorulev/homework_8/script.js
+++ src/a.gorulev/homework_8/script.js
@@ -8,13 +8,16 @@ class StopWatch {
   }
 
   get formattedTime() {
-    const _seconds = Math.floor(this.time / 1000) ;
-		const _minutes = Math.floor(_seconds / 60);
-		const hours = Math.floor(_minutes / 60) + '';
-    const minutes = _minutes % 60 + '';
-    const seconds = _seconds % 60 + '';
-		const miliSeconds = this.time % 1000 + '';
-    return `${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}.${miliSeconds.padStart(3, '0')}`;
+    const _seconds = Math.floor(this.time / 1000);
+    const _minutes = Math.floor(_seconds / 60);
+    const hours = Math.floor(_minutes / 60) + "";
+    const minutes = (_minutes % 60) + "";
+    const seconds = (_seconds % 60) + "";
+    const miliSeconds = (this.time % 1000) + "";
+    return `${hours.padStart(2, "0")}:${minutes.padStart(
+      2,
+      "0"
+    )}:${seconds.padStart(2, "0")}.${miliSeconds.padStart(3, "0")}`;
   }
 
   start() {
@@ -25,22 +28,22 @@ class StopWatch {
     this.intervalId = setInterval(() => {
       this.time -= Date.now() - this.prevTime;
       this.prevTime = Date.now();
-			if (this.time <= 0) {
-				this.time = 0;
-				this.stop() ;
-			} 
+      if (this.time <= 0) {
+        this.time = 0;
+        this.stop();
+      }
       this.render();
     }, 5);
   }
 
-	stop() {
+  stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
   }
 
   reset() {
     this.prevTime = Date.now();
-		this.time = this.min * 5;
+    this.time = this.min * 5;
     this.render();
   }
 
