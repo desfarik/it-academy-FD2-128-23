69edc6b6007a39147f8b5dda6960e7c292fc217e
Shchuka-Vladimir <vovtut.by@gmail.com>
2023-04-16 12:05:36 +0300

    v.shchuka/homework_8

--- src/v.shchuka/homework/homework_8/js/timer.js
+++ src/v.shchuka/homework/homework_8/js/timer.js
@@ -1,7 +1,8 @@
 class Timer {
   time = 0;
-  constructor(element) {
+  constructor(element, container) {
     this.element = element;
+    this.container = container;
     this.render();
   }
   start() {
@@ -9,9 +10,20 @@ class Timer {
       return;
     }
     this.previousTime = Date.now();
+    this.showActiveStop();
+    this.getIntervalId();
+  }
+  showActiveStop() {
+    if (this.time !== 0) {
+      this.container.classList.add("stop-active");
+      this.container.classList.remove("play-active");
+    }
+  }
+  getIntervalId() {
     this.intervalId = setInterval(() => {
       if (this.time <= 0) {
         this.stop();
+        this.container.classList.remove("play-active");
         return;
       }
       this.time -= Date.now() - this.previousTime;
@@ -33,22 +45,50 @@ class Timer {
   stop() {
     clearInterval(this.intervalId);
     this.intervalId = null;
+    this.container.classList.remove("stop-active");
+    this.container.classList.add("play-active");
   }
   reset() {
     this.time = 0;
     this.stop();
+    this.container.classList.remove("play-active");
     this.render();
   }
   set(time) {
+    this.container.classList.add("play-active");
     this.previousTime = Date.now();
     this.time = time * 1000 * 60;
     this.render();
   }
   addTime(time) {
+    this.container.classList.add("play-active");
     this.previousTime = Date.now();
     this.time += time * 1000 * 60;
     this.render();
   }
+  onClick(event) {
+    const target = event.target;
+    if (target.tagName !== "BUTTON") {
+      return;
+    } else if (target.hasAttribute("data-add-time")) {
+      this.addTime(target.dataset.addTime);
+    } else if (target.hasAttribute("data-set")) {
+      this.set(target.dataset.set);
+    } else if (target.classList.contains("control-button")) {
+      this.control(target);
+    }
+  }
+  control(target) {
+    if (target.classList.contains("button-play")) {
+      this.start();
+    } else if (target.classList.contains("button-stop")) {
+      this.stop();
+    } else if (target.classList.contains("button-reset")) {
+      this.reset();
+    }
+  }
 }
 const elementTimer = document.querySelector(".timer");
-const timer = new Timer(elementTimer);
+const timerContainer = document.querySelector(".timer-container");
+const timer = new Timer(elementTimer, timerContainer);
+timerContainer.addEventListener("click", () => timer.onClick(event));
