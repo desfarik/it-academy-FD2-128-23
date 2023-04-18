392003f8b2918dcf6a7e7ef380644a2e42ff7c52
Dima Astreyko <desfarik@gmail.com>
2023-04-18 21:22:55 +0300

    lesson 18

--- src/d.astreyko/lesson_18/calendar/current_date.js
+++ src/d.astreyko/lesson_18/calendar/current_date.js
@@ -1,5 +1,4 @@
 const date = new Date();
-const WEEK_DAYS = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"]
 const weekday = WEEK_DAYS[date.getDay()];
 const day = date.getDate();
 const currDate = document.querySelector('.current-date');
