61de68a4dededd54cf60bbb4a9b4d064a9bec0eb
Vitaliy24011980 <6663213@gmail.com>
2023-05-02 17:11:45 +0300

    v.shcherbakov/homework_9

---
+++ src/v.shcherbakov/homeworks/homework_9/constants.js
@@ -0,0 +1,15 @@
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+const WEEK_DAYS = ["MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY","SUNDAY"]
