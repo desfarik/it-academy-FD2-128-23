eeb31b1aadaa0349edbd4a21431b6cf5244f9fc9
Dima Astreyko <desfarik@gmail.com>
2023-05-19 21:24:47 +0300

    angular 2

---
+++ src/d.astreyko/angular/angular_1/src/app/components/event-loop.js
@@ -0,0 +1,21 @@
+function log(object) {
+  console.log(object)
+}
+
+log('1');
+log('2');
+button;
+
+button.addEventListener('click')
+
+event.stopPropagation()
+event.stopImmediatePropagation();
+
+button.addEventListener('click')
+setTimeout(()=>log('3'))
+
+Promise.resolve().then(()=>{
+  log('4')
+})
+
+log('5');
